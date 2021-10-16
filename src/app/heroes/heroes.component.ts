170be80b388cfdeab041d24766920a66e2b52f07
cspencer-queue <coreyspencer7@gmail.com>
2021-10-16 01:10:28 -0400

    3 files modified, 3 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -8,8 +8,10 @@ import { HEROES } from '../mock-heroes';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
+  
   heroes = HEROES;
-  selectedHero?: Hero;
+  
+  selectedHero!: Hero;
 
   constructor() { }
 
