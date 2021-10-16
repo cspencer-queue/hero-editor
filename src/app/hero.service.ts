de0779d8dbb7ca882c0af1bdd9f3de086ead1117
cspencer-queue <coreyspencer7@gmail.com>
2021-10-16 10:37:33 -0400

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,20 +1,21 @@
 import { Injectable } from '@angular/core';
+
+import { Observable, of } from 'rxjs';
+
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
-import { Observable, of } from 'rxjs';
 import { MessageService } from './message.service';
 
 @Injectable({
   providedIn: 'root',
 })
-
-getHeroes(): Observable<Hero[] {
-  const heroes = of(HEROES);
-  this.messageService.add('HeroService: fetched heroes');
-  return heroes;
-}
 export class HeroService {
 
   constructor(private messageService: MessageService) { }
-
+  
+  getHeroes(): Observable<Hero[]> {
+    const heroes = of(HEROES);
+    this.messageService.add('HeroService: fetched heroes');
+    return heroes;
+  }
 }
