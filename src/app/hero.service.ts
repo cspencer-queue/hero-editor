a901a16f71d3fbafdd942d8f3038ed8504096c77
cspencer-queue <coreyspencer7@gmail.com>
2021-10-16 02:07:15 -0400

    1 file modified

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -1,8 +1,14 @@
 import { Injectable } from '@angular/core';
+import { Hero } from './hero';
+import { HEROES } from './mock-heroes';
 
 @Injectable({
   providedIn: 'root',
 })
+
+getHeroes(): Hero[] {
+  return HEROES;
+}
 export class HeroService {
 
   constructor() { }
