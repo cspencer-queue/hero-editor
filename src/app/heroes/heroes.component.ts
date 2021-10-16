ed0b8521f0498116eda5f4103d9c31f67d0f266e
cspencer-queue <coreyspencer7@gmail.com>
2021-10-16 10:28:02 -0400

    1 file modified

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -9,10 +10,10 @@ import { HeroService } from '../hero.service';
 })
 export class HeroesComponent implements OnInit {
   
+  selectedHero?: Hero;
+
   heroes: Hero[] = [];
   
-  selectedHero!: Hero;
-
   constructor(private heroService: HeroService) {}
 
   ngOnInit() {
